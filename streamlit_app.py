04c634a47aaa4d76c9753180323d0d83ab1d88d5
Dominique Saputro <101784914+dominique-saputro@users.noreply.github.com>
2025-06-05 12:11:16 +0000

    XML Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,23 @@
 import streamlit as st
+import pandas as pd
 
+with st.form('my_form'):
+   st.write('Extract Errors')
+   xml_string = st.text_area('XML STRING')
+   err_string = st.text_area('ERROR LINES')
+   st.form_submit_button('Run')
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   my_number = st.slider('Pick a number', 1, 10)
-   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
-   st.form_submit_button('Submit my picks')
+xml_lines = xml_string.splitlines()
+err_lines = err_string.splitlines()
+nik = []
+for i in err_lines:
+   x = int(i)-2
+   res_line = xml_lines[x]
+   nik_string = res_line[22:38]
+   nik.append(nik_string)
+
+df = pd.DataFrame(
+    nik, columns=("col %d" % i for i in range(1))
+)
+
+st.table(df)
