5b853325154c37b0284a37afb757e7837439bd96
Dominique Saputro <101784914+dominique-saputro@users.noreply.github.com>
2025-06-05 12:10:37 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   my_number = st.slider('Pick a number', 1, 10)
+   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   st.form_submit_button('Submit my picks')
