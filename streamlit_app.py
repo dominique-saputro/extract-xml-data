275e58a07ae6aa28e22c4848ffa32082d02d8b10
Dominique Saputro <101784914+dominique-saputro@users.noreply.github.com>
2025-06-09 10:44:22 +0700

    Update streamlit_app.py
    Changed XML data to file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,16 @@ import pandas as pd
 
 with st.form('my_form'):
     st.write('Extract Errors')
-    xml_string = st.text_area('XML STRING')
+    xml_file = st.file_uploader('XML File')
     err_string = st.text_area('ERROR LINES')
     submitted = st.form_submit_button('Run')
 
 # Only process data if form is submitted and inputs are non-empty
-if submitted and xml_string and err_string:
-    xml_lines = xml_string.splitlines()
+if submitted and xml_file and err_string:
+    # Read the XML file
+    xml_bytes = xml_file.read()
+    xml_text = xml_bytes.decode('utf-8')
+    xml_lines = xml_text.splitlines()
     err_lines = err_string.splitlines()
     nik = []
 
@@ -28,4 +31,4 @@ if submitted and xml_string and err_string:
     df = pd.DataFrame(nik, columns=["Extracted NIK"])
     st.table(df)
 else:
-    st.write("Please submit the form to process data.")
+    st.write("Please upload an XML file and enter error lines to extract NIKs.")
